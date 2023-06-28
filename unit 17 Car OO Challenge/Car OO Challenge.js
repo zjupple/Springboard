267c27950753216335840b1aa9b54e1961cf5170
jupple <zjfontenot@gmail.com>
2023-06-28 00:53:19 -0700

    car OO 1st attempt

---
+++ unit 17 Car OO Challenge/Car OO Challenge.js
@@ -0,0 +1,57 @@
+Car OO Challenge.js
+
+// What I think this means???
+
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }  
+    honk() {
+        return "Beep";
+    }
+    toString() {
+        return `The cool vehicle is a ${this.make},
+         ${this.model}, built in ${this.year}`;
+    }
+
+
+};
+
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+    // ??? do i need to reference the constructor keyword in this
+    // subclass or is super enough
+        super(make, model, year)
+
+        this.numWheels = 4;
+    }
+};
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year)
+
+        this.numWheels = 2;
+
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+};
+
+class Garage extends Vehicle {
+    constructor(vehicles, capacity) {
+        super(make, model, year)
+
+        this.vehicles = [];
+        this.capacity = 7;
+    }
+    // i know there is some logic for here
+    add() {
+
+    }
+    
+}
